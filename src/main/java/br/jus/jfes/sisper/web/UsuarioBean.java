322796b44dfb92283e8f7fa6369203cefdbe6ca4
Marcos Rosa <marcosrosa@jfes.jus.br>
2014-08-15 16:59:30 -0300

    Commit para integracao do repositorio

--- src/main/java/br/jus/jfes/sisper/web/UsuarioBean.java
+++ src/main/java/br/jus/jfes/sisper/web/UsuarioBean.java
@@ -1,37 +1,34 @@
 package br.jus.jfes.sisper.web;
 
-import java.io.Serializable;
-
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-//import javax.portlet.PortletPreferences;
-//import javax.portlet.PortletRequest;
+import javax.portlet.PortletRequest;
 
 @ManagedBean(name = "usuarioBean")
 @SessionScoped
-public class UsuarioBean implements Serializable {
+public class UsuarioBean extends BaseAction {
 	
-	//administradores fica no preferences
-	public static final String PREF_GRUPO = "nomeGrupoAdministradores";
-	public static final String GRUPO_PADRAO = "AdministradoresInspecao";
-	
-	String nomeGrupoAdministrador;
-		
-	public UsuarioBean(){
-		/*FacesContext ctx = FacesContext.getCurrentInstance();
-		PortletRequest req = (PortletRequest) ctx.getExternalContext().getRequest();
-		PortletPreferences prefs = req.getPreferences();
-		if(prefs != null){
-			nomeGrupoAdministrador = prefs.getValue(PREF_GRUPO, GRUPO_PADRAO);
-		}*/ 
+	public UsuarioBean() {
+		//so pra atender api		
 	}
 	
+	//administradores fica no preferences
+	// nao tem um grupo de administradores do sisper somente de usuarios.
+	public static final String PREF_GRUPO = "GrupoSisper";
+	public static final String GRUPO_PADRAO = "usuarios_sisper";
+	
+	String nomeGrupoAdministrador;
+	
+	@PostConstruct
+	public void inicializar() {
+		//busca diretamente do baseAction.
+		nomeGrupoAdministrador = getPortletPreference(PREF_GRUPO, GRUPO_PADRAO);
+	}
+		
 	public boolean isAdministrador(){
-		//FacesContext ctx = FacesContext.getCurrentInstance();
-		//PortletRequest req = (PortletRequest) ctx.getExternalContext().getRequest();
-		//return  req.isUserInRole(nomeGrupoAdministrador);  
-		return true;
+		PortletRequest req = getPortletRequest();
+		return  req.isUserInRole(nomeGrupoAdministrador);
 	}
 	
 }
